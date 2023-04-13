d2b3f8083788866f4bdd92c77d82ad00685b3917
IrfanAlfiansyah <irfan.alfiansyah1995@gmail.com>
2023-04-13 13:49:01 +0700

    update

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 
-	fmt.Println("aplikasi berjalan pada port 8081")
+	fmt.Println("Your aplication run on port 8081")
 
 	if err := command.Run(os.Args[1:]); err != nil {
 		log.Fatal(err)
